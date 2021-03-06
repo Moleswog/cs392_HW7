d113113897123cdc85d9948ee6029cb05461ab92
Grayson Molesworth <moleswog@oregonstate.edu>
2021-03-06 13:19:55 -0800

    Re-Write Test 1: Count 100 Lines

--- Q1/test_q1.py
+++ Q1/test_q1.py
@@ -5,7 +5,7 @@ class HW7Test(unittest.TestCase):
     def test_Count100(self):
         # test to ensure 100 lines are printed
         result = q1.run(100)
-        self.assertEqual(result.count("\n"), 100)
+        self.assertEqual(result, 100)
 
 
 
