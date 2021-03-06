b80b8914eceb68db8ecb0ad8d2e2a289e012752b
Grayson Molesworth <moleswog@oregonstate.edu>
2021-03-06 13:43:04 -0800

    implemented PrintFunc(): takes input and prints output based on program specs

--- Q1/q1.py
+++ Q1/q1.py
@@ -4,3 +4,18 @@ def run(x):
         i+=1
         print(i)
     return i
+
+def PrintFunc(x):
+    if(type(x) != int or x < 0):
+        return (-1)
+    else:
+        out = x
+        if(x%3 == 0):
+            out = "Fizz"
+        if(x%5 == 0):
+            out = "Buzz"
+        if(x%3 == 0 and x%5 == 0):
+            out = "FizzBuzz"
+
+        print(out)
+        return out
